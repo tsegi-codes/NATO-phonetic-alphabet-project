6395ebea057c73bd306c2c30b75477d893a1579d
tsegi-codes <enyew.betiamare@gmail.com>
2026-05-08 12:48:46 +0200

    NATO phonetic alphabet project

---
+++ main.py
@@ -0,0 +1,39 @@
+# student_dict = {
+#     "student": ["Angela", "James", "Lily"],
+#     "score": [56, 76, 98]
+# }
+#
+# #Looping through dictionaries:
+# for (key, value) in student_dict.items():
+#     #Access key and value
+#     pass
+#
+# import pandas
+# student_data_frame = pandas.DataFrame(student_dict)
+#
+# #Loop through rows of a data frame
+# for (index, row) in student_data_frame.iterrows():
+#     #Access index and row
+#     #Access row.student or row.score
+#     pass
+#
+# # Keyword Method with iterrows()
+# # {new_key:new_value for (index, row) in df.iterrows()}
+import pandas as pd
+# #TODO 1. Create a dictionary in this format:
+# {"A": "Alfa", "B": "Bravo"}
+
+nato = pd.read_csv("nato_a.csv")
+print(nato)
+
+nato_dict = {row.letter:row.code for (index, row) in nato.iterrows()}
+print(nato_dict)
+
+
+# #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+user_input = input("Enter your word or phrase: ").upper()
+# for letter in user_input:
+#     pass
+nato_list = [nato_dict[letter] for letter in user_input if letter in nato_dict]
+print(nato_list)
+
